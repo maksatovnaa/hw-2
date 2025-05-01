d60843441af0ef9dcccb10c2cbba83f3b0d0f00c
maksatovnaa <sofiyanur.tazhmurat@jihc.edu.kz>
2025-05-01 17:28:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.burger').addEventListener('click',function(){
+
+  document.querySelector('nav').classList.toggle('active');
+  document.querySelector('.burger').classList.toggle('burger-active');
+
+});
